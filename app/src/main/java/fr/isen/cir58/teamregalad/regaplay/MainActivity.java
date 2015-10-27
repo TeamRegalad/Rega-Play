62ce9da8d429076652f78539ee58639938a6dd85
asukero <thomas.fossati59@gmail.com>
2015-10-27 01:25:21 +0100

    Cleaned MainActivity and activity_main.xml to be blank for everyone

--- app/src/main/java/fr/isen/cir58/teamregalad/regaplay/MainActivity.java
+++ app/src/main/java/fr/isen/cir58/teamregalad/regaplay/MainActivity.java
@@ -22,23 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File sdcard = new File("/sdcard1/Musique/Textures");
-        Log.d("TestA",String.valueOf(sdcard.exists()));
-        File track = new File("/sdcard1/Musique/Textures/2011 Dualism (320 kbps)/03. Reaching Home.mp3");
-        Log.d("TestF",track.getPath());
-        Log.d("Test", Environment.getExternalStorageDirectory().getAbsolutePath());
-        Log.d("Test", String.valueOf(track.exists()));
-        Uri trackURI = Uri.fromFile(track);
-        MediaPlayer mp = new MediaPlayer();
-        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try{
-            mp.setDataSource(getApplicationContext(), trackURI);
-            mp.prepare();
-            mp.start();
-        } catch (IOException e){
-            Log.e("ERROR",e.getMessage());
-        }
-
     }
 
 }

a698d643b16cc0216810b5dd0e4da6a5d8a12420
Valryo <papattyn@gmail.com>
2015-11-01 14:57:07 +0100

    First solution for twitter

--- app/src/main/java/fr/isen/cir58/teamregalad/regaplay/social/shareMusicInfo.java
+++ app/src/main/java/fr/isen/cir58/teamregalad/regaplay/social/shareMusicInfo.java
@@ -1,8 +1,11 @@
 package fr.isen.cir58.teamregalad.regaplay.social;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.widget.Toast;
 
 /**
  * Created by Paul on 10/26/2015.
@@ -28,5 +31,30 @@ public class shareMusicInfo {
             activity.startActivity(intent);
         }
 
+    }
+
+    public static void sendTweet(Activity activity, String musicInfo)
+    {
+        final PackageManager packageManager = activity.getPackageManager();
+        final Context context = activity.getApplicationContext();
+
+        try
+        {
+            // Check if the Twitter app is installed on the phone.
+            packageManager.getPackageInfo("com.twitter.android", 0);
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setClassName("com.twitter.android", "com.twitter.android.composer.ComposerActivity");
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, musicInfo);
+            activity.startActivity(intent);
+
+        }
+        catch (Exception e) // If the app is not installed, open twitter with the browser
+        {
+            Toast.makeText(context, "Twitter is not installed on this device", Toast.LENGTH_SHORT).show();
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("https://twitter.com/intent/tweet?text=%s", musicInfo)));
+            activity.startActivity(browserIntent);
+        }
     }
 }

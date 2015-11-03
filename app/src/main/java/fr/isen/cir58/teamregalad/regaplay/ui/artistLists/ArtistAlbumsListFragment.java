d3e8009792c9e137e478b859d937b69c67302298
asukero <thomas.fossati59@gmail.com>
2015-11-03 18:40:38 +0100

    Tabs fragments and lorem ipsum added for debug

---
+++ app/src/main/java/fr/isen/cir58/teamregalad/regaplay/ui/artistLists/ArtistAlbumsListFragment.java
@@ -0,0 +1,22 @@
+package fr.isen.cir58.teamregalad.regaplay.ui.artistLists;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.isen.cir58.teamregalad.regaplay.R;
+
+/**
+ * Created by Thomas Fossati on 03/11/2015.
+ */
+public class ArtistAlbumsListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.artist_albums_list_fragment, container, false);
+
+        return rootView;
+    }
+}

704fe759078490c67f51529e91456a000f1c4219
asukero <thomas.fossati59@gmail.com>
2015-11-05 00:36:41 +0100

    Replaced Picasso with Glide

--- app/src/main/java/fr/isen/cir58/teamregalad/regaplay/MainActivity.java
+++ app/src/main/java/fr/isen/cir58/teamregalad/regaplay/MainActivity.java
@@ -5,10 +5,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;

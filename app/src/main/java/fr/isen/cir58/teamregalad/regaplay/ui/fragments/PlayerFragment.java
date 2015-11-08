da7d1660756d5766bfe54edaf1d2700ce087c3fc
asukero <thomas.fossati59@gmail.com>
2015-11-09 00:49:21 +0100

    Added getDuration method for progressBar and timer

--- app/src/main/java/fr/isen/cir58/teamregalad/regaplay/ui/fragments/PlayerFragment.java
+++ app/src/main/java/fr/isen/cir58/teamregalad/regaplay/ui/fragments/PlayerFragment.java
@@ -141,4 +141,21 @@ public class PlayerFragment extends Fragment implements View.OnClickListener {
                 ShareMusicInfo.shareVia(getActivity(),this.song.toString());
         }
     }
+    public static String getDuration(long milliseconds) {
+        long sec = (milliseconds / 1000) % 60;
+        long min = (milliseconds / (60 * 1000))%60;
+        long hour = milliseconds / (60 * 60 * 1000);
+
+        String s = (sec < 10) ? "0" + sec : "" + sec;
+        String m = (min < 10) ? "0" + min : "" + min;
+        String h = "" + hour;
+
+        String time = "";
+        if(hour > 0) {
+            time = h + ":" + m + ":" + s;
+        } else {
+            time = m + ":" + s;
+        }
+        return time;
+    }
 }

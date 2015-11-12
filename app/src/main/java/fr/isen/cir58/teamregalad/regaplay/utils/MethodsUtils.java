41557c2a04683a52d2805a1037844fff98027392
asukero <thomas.fossati59@gmail.com>
2015-11-12 03:11:47 +0100

    static methods like get duration in string or convert dp to px

---
+++ app/src/main/java/fr/isen/cir58/teamregalad/regaplay/utils/MethodsUtils.java
@@ -0,0 +1,55 @@
+package fr.isen.cir58.teamregalad.regaplay.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by Thomas Fossati on 11/11/2015.
+ */
+public class MethodsUtils {
+
+    public static String getDuration(long milliseconds) {
+        long sec = (milliseconds / 1000) % 60;
+        long min = (milliseconds / (60 * 1000)) % 60;
+        long hour = milliseconds / (60 * 60 * 1000);
+
+        String s = (sec < 10) ? "0" + sec : "" + sec;
+        String m = (min < 10) ? "0" + min : "" + min;
+        String h = "" + hour;
+
+        String time = "";
+        if (hour > 0) {
+            time = h + ":" + m + ":" + s;
+        } else {
+            time = m + ":" + s;
+        }
+        return time;
+    }
+
+    /**
+     * This method converts dp unit to equivalent pixels, depending on device density.
+     *
+     * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent px equivalent to dp depending on device density
+     */
+    public static int convertDpToPixel(float dp, Context context){
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return (int)((dp * displayMetrics.density) + 0.5);
+    }
+
+    /**
+     * This method converts device specific pixels to density independent pixels.
+     *
+     * @param px A value in px (pixels) unit. Which we need to convert into db
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent dp equivalent to px value
+     */
+    public static float convertPixelsToDp(float px, Context context){
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return (int) ((px/displayMetrics.density)+0.5);
+    }
+
+
+}
